46b32e0f291dd8f4c75255e71dee230a3a7b5ae1
tracht <marija.trachtenberg@gmail.com>
2020-10-05 10:46:13 +0100

    Renames/writes test for Multiple of 3 andn 5

---
+++ MultiplesOf3and5/MultiplesOf3and5.test.js
@@ -0,0 +1,8 @@
+const getSumOfNumberUsingMultiplesOf3and5 = require('./MultiplesOf3and5');
+
+describe('getSumOfNumberUsingMultiplesOf3and5', () => {
+  it('input of 20 returns a sum of 78', () => {
+    expect(getSumOfNumberUsingMultiplesOf3and5(20)).toEqual(78);
+  })
+});
+
